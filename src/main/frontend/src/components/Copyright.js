0a79a7923b9801d69a7c35a5abbf0f856fc8ee88
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2021-10-19 23:00:57 -0400

    Created SignUp Frontend

--- src/main/frontend/src/components/Copyright.js
+++ src/main/frontend/src/components/Copyright.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Paper, withStyles, Grid, TextField, Button, FormControlLabel, Checkbox } from '@material-ui/core';
-import { Face, Fingerprint } from '@material-ui/icons'
+import { Paper, withStyles, Typography, TextField, Button } from '@material-ui/core';
+import AccountService from '../services/AccountService';
+
 const styles = theme => ({
     margin: {
         margin: theme.spacing.unit * 2,
@@ -12,55 +13,78 @@ const styles = theme => ({
 
 class LoginTab extends React.Component {
 
-  constructor(props){
-    super(props);
-    this.state ={username: '', password: ''};
-    this.submit = this.submit.bind(this)
+  
+  constructor(props) {
+    super(props)
+
+    this.onChangeUserName = this.onChangeUserName.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+        username: '',
+        password: ''
+    }
+}
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const account = {
+      username: this.state.username,
+      password: this.state.password,
+    }
+
+    AccountService.createAccount(account)
   }
 
-  submit(event){
-    alert('Username' + this.state.username + 'Password' + this.state.password);
+    onChangeUserName(e) {
+      this.setState({ username: e.target.value })
   }
+
+  onChangePassword(e) {
+      this.setState({ password: e.target.value })
+  }
+
+  handleChange = e => {
+    this.setState({ [e.currentTarget.id]: e.currentTarget.value });
+  };
 
     render() {
         const { classes } = this.props;
-        return (
-            <Paper className={classes.padding}>
-                <div className={classes.margin}>
-                    <Grid container spacing={8} alignItems="flex-end">
-                        <Grid item>
-                            <Face />
-                        </Grid>
-                        <Grid item md={true} sm={true} xs={true}>
-                            <TextField id="username" value={this.state.username} label="Username" type="email" fullWidth autoFocus required />
-                        </Grid>
-                    </Grid>
-                    <Grid container spacing={8} alignItems="flex-end">
-                        <Grid item>
-                            <Fingerprint />
-                        </Grid>
-                        <Grid item md={true} sm={true} xs={true}>
-                            <TextField id="password" value={this.state.password} label="Password" type="password" fullWidth required />
-                        </Grid>
-                    </Grid>
-                    <Grid container alignItems="center" justify="space-between">
-                        <Grid item>
-                            <FormControlLabel control={
-                                <Checkbox
-                                    color="primary"
-                                />
-                            } label="Remember me" />
-                        </Grid>
-                        <Grid item>
-                            <Button disableFocusRipple disableRipple style={{ textTransform: "none" }} variant="text" color="primary">Forgot password ?</Button>
-                        </Grid>
-                    </Grid>
-                    <Grid container justify="center" style={{ marginTop: '10px' }}>
-                        <Button variant="outlined" color="primary" style={{ textTransform: "none" }}>Login</Button>
-                    </Grid>
-                </div>
-            </Paper>
-        );
+        return( <div >
+          <Paper className={classes.root}>
+              <form className={classes.container} onSubmit={this.handleSubmit} >
+                  <TextField                    
+                      id={"username"}
+                      placeholder="username"
+                      onInput={this.onChangeUserName}
+                      onChange={this.handleChange}
+                      autoComplete="off"
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                  />
+                  <TextField                      
+                      id={"password"}
+                      placeholder="password"
+                      onInput={this.onChangePassword}
+                      onChange={this.handleChange}
+                      autoComplete="off"
+                      formControlProps={{
+                        fullWidth: true
+                      }}                     
+                  />
+                  <Typography className={classes.divider} />
+                  <Button
+                      type="submit"
+                      className={classes.button}
+                  >Login
+                  </Button>
+              </form>
+          </Paper>
+      </div>
+  );
     }
 }
 
--- src/main/frontend/src/services/AccountService.js
+++ src/main/frontend/src/services/AccountService.js
@@ -5,11 +5,11 @@ const ACCOUNT_API_BASE_URL = 'http://localhost:7070/list';
 class AccountService{
 
     getEmployees(){
-        return axios.get(ACCOUNT_API_BASE_URL + "/" + "list");
+        return axios.get(ACCOUNT_API_BASE_URL + '/' + 'list');
     }
 
     createAccount(account){
-        return axios.post(ACCOUNT_API_BASE_URL + "/" + "create", account);
+        return axios.post("http://localhost:7070/create", account);
     }
 }
 
