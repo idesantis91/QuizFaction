4b378398dc641ec20568410e77bd8561811c91e7
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2021-11-02 23:01:59 -0400

    Added Frontend routing

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,12 +1,42 @@
 import './App.css';
 import React from 'react';
 import CreateTab from './components/CreateTab';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-        <CreateTab/>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Create Account</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <CreateTab />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
